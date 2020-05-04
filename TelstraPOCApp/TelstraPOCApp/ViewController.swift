2244746f4d080a54c0e7c02154ee154889ea9159
Nikhil Wagh <NW00594382@techmahindra.com>
2020-05-04 19:14:31 +0530

    Initial commit.

---
+++ TelstraPOCApp/TelstraPOCApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelstraPOCApp
+//
+//  Created by Nikhil Wagh on 5/4/20.
+//  Copyright © 2020 Tech Mahindra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
